c071b1e183e90460be6d420c9ba49cd86cc80c39
Talel Ben Belgacem <58850422+Talel12@users.noreply.github.com>
2022-09-10 13:27:46 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import ProductCrad from './ProductCrad';
 
 function App() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    axios.get("https://la-mode.shop/api/products?ws_key=SH97SWJXX4NIXZ8ZELVFQ1EXZERWMT8N&output_format=JSON").then(result =>
+      setProducts(result.data.products))
+  }, []);
+  
+  console.log(products)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {products.map(el=><ProductCrad element={el}/>)}
     </div>
   );
 }
